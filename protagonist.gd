ffab7d28e54b5539cfa05f7e7445fc0ab58dfaae
Myclfi <mmicha.shapiro@gmail.com>
2026-02-28 18:31:01 -0700

    Add coyote timer and adjust protagonist physics
    Define collision polygons for tiles in basic_scene.tscn, add a CoyoteTimer node and connection to the Protagonist, and tweak rendering setting in project.godot (disable default texture filter). In protagonist.gd reduce movement/jump constants, add falling/floating state variables, refactor gravity into handle_gravity(), start the coyote timer when leaving the floor, and handle timeout to enable falling — implementing a short "coyote time" jump grace period and general physics tuning.

--- protagonist.gd
+++ protagonist.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 75.0
+const JUMP_VELOCITY = -200.0
 
+var falling = true;
+var floating = false;
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	handle_gravity(delta);
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,3 +24,18 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func handle_gravity(delta: float): 
+	if !is_on_floor():
+		if !floating:
+			floating = true;
+			$CoyoteTimer.start();
+	else:
+		falling = false;
+		floating = false;
+	if falling:
+		velocity += get_gravity() * delta;
+
+
+func _on_coyote_timer_timeout() -> void:
+	falling = true;
