dae24e8deecf958fe0bf98f4db295af394d6439c
Myclfi <mmicha.shapiro@gmail.com>
2026-03-01 12:01:54 -0700

    Added Rudimentary Death Screen

--- monster.gd
+++ monster.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_monster_collider_body_entered(body: Node2D) -> void:
+	print("Body Entered: " + str(body))
+	if body in get_tree().get_nodes_in_group("Protagonist"):
+		get_tree().change_scene_to_file("res://death_screen.tscn")
